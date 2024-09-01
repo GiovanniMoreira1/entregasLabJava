1762507cb2a1f3b483f8129ceabe6196097d9453
GiovanniMoreira1 <giovannimoreira131@gmail.com>
2024-09-01 17:59:17 -0300

    exercicio 3 pronto

--- lab04exe03/src/main/java/com/mycompany/lab04exe03/Main.java
+++ lab04exe03/src/main/java/com/mycompany/lab04exe03/Main.java
@@ -11,8 +11,9 @@ public class Main {
     public static void main(String[] args) {
         ArrayList<Pessoa> agenda = new ArrayList<>();
         Scanner input = new Scanner(System.in);
-        
-        while(true) {
+        boolean loop = true;
+
+        while(loop) {
             System.out.println("-------------------------------------");
             System.out.println("n - Criar");
             System.out.println("d - Deletar");
@@ -23,10 +24,46 @@ public class Main {
             String opcao = input.nextLine();
             
             switch(opcao) {
-                case "n" -> System.out.println("Opção escolhida - Criar Pessoa"); 
-                case "d" -> System.out.println("Opção escolhida - Deletar Pessoa");
-                case "p" -> System.out.println("Opção escolhida - Mostrar agenda");
-                case "q" -> System.out.println("Opção escolhida - Sair");
+                case "n" -> {
+                    System.out.println("Opção escolhida - Criar Pessoa");
+                    System.out.println("Digite o nome da pessoa: ");
+                    String nome = input.nextLine();
+                    System.out.println("Digite o número do telefone de " + nome);
+                    String telefone = input.nextLine();
+
+                    Pessoa pessoa = new Pessoa(nome, telefone);
+
+                    agenda.add(pessoa);
+                }
+                case "d" -> {
+                    System.out.println("Opção escolhida - Deletar Pessoa");
+                    System.out.println("Digite o nome da pessoa que será deletada de sua agenda: ");
+                    String nome = input.nextLine();
+
+                    for (int i = 0; i < agenda.size(); i++) {
+                        Pessoa pessoa = agenda.get(i);
+                        if(nome.equals(pessoa.getNome())) {
+                            agenda.remove(i);
+                            i--;
+                        }
+                    }
+                    System.out.println("Contato removido com sucesso!");
+                }
+                case "p" -> {
+                    System.out.println("Opção escolhida - Mostrar agenda");
+                    System.out.println("-----------------------------------");
+
+                    for (Pessoa pessoa : agenda) {
+                        System.out.println("Nome: " + pessoa.getNome());
+                        System.out.println("Telefone: " + pessoa.getTelefone());
+                        System.out.println("ID: " + Pessoa.getId());
+                        System.out.println("-----------------------------------");
+                    }
+                }
+                case "q" -> {
+                    System.out.println("Opção escolhida - Sair");
+                    loop = false;
+                }
                 default -> System.out.println("Opção inválida - tente novamente");
             }
         }
--- lab04exe03/src/main/java/com/mycompany/lab04exe03/Pessoa.java
+++ lab04exe03/src/main/java/com/mycompany/lab04exe03/Pessoa.java
@@ -5,16 +5,28 @@
 package com.mycompany.lab04exe03;
 
 /* @author unifgimoreira */
+
 public class Pessoa {
     private String nome, telefone;
-    private int id;
+    private static int id = 0;
     
-    public Pessoa(String nome, String telefone, int id) {
+    public Pessoa(String nome, String telefone) {
         this.nome = nome;
         this.telefone = telefone;
-        this.id = id;
     }
     
     public Pessoa() {
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public static int getId() {
+        return id++;
     }
 }
